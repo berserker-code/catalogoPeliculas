776c30ecd6f5e0a0736771570be4749aeec10df3
mau <mauriciohramirez46@gmail.com>
2026-08-31 14:19:35 -0500

    agrega buscador de peliculas con debounce

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:catalogopeliculas/models/movie.dart';
 import 'package:catalogopeliculas/screens/favorites_screen.dart';
 import 'package:catalogopeliculas/screens/movie_detail_screen.dart';
@@ -14,6 +16,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final MovieService _movieService = MovieService();
   List<Movie> movies = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  Timer? _debounce;
+  bool isLoading = true;
+  String? errorMessage;
+
 
   @override
   void initState() {
@@ -21,22 +29,74 @@ class _HomeScreenState extends State<HomeScreen> {
     _movieService.getPopularMovies().then((data) {
       setState(() {
         movies = data;
+        isLoading = false;
+      });
+    }).catchError((error){
+      setState(() {
+        errorMessage = 'no se cargaron las peliculas';
+        isLoading = false;
       });
     });
+  }
+
+  @override
+  void dispose(){
+    _debounce?.cancel();
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged(String query){
+    if(_debounce?.isActive ?? false )  _debounce!.cancel();
+  _debounce = Timer(const Duration(milliseconds: 500), (){
+    if (query.isEmpty){
+          _movieService.getPopularMovies().then((data) {
+      setState(() {
+        movies = data;
+        isLoading = false;
+      });
+    });
+    }else{
+          _movieService.searchMovies(query).then((data) {
+      setState(() {
+        movies = data;
+        isLoading = false;
+      });
+    }).catchError((error){
+      setState(() {
+        errorMessage = 'no se cargaron las peliculas';
+        isLoading = false;
+      });
+    });
+    }
+  });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(title: Text('data'),
+      appBar: AppBar(title: TextField(
+        controller: _searchController,
+        onChanged: _onSearchChanged,
+        decoration: InputDecoration(
+          hintText: 'buscar pelicula',
+          border: InputBorder.none,
+        ),
+        style: TextStyle(color: Colors.black,),
+      ) ,
       actions: [
         IconButton(onPressed: (){
           Navigator.push(context, MaterialPageRoute(builder: (context) =>FavoritesScreen()));
         }, icon: Icon(Icons.favorite, color: Colors.red,))
       ],),
 
-      body: ListView.builder(
+
+      body: isLoading
+      ? Center(child: CircularProgressIndicator())
+      : errorMessage != null
+        ? Center(child: Text(errorMessage!))
+        : ListView.builder(
         itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -27,4 +27,25 @@ class MovieService {
        throw Exception('error');
     }
   }
+
+    Future<List<Movie>> searchMovies( String query) async {
+    final url = Uri.https(_baseUrl, '/3/search/movie', {
+      'query': query,
+      'api_key': apikey,
+      'language': 'es-ES',
+      'page': '1',
+    });
+
+    final response = await http.get(url);
+      if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      List<dynamic> results = data['results'];
+      List<Movie> movies = results
+          .map((movieJson) => Movie.fromJson(movieJson))
+          .toList();
+      return movies;
+    } else {
+       throw Exception('error');
+    }
+    }
 }
